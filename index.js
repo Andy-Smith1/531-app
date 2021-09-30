c30dab917ed600aba6917dbb388e623d7f3f0f06
Andy Smith <andy151993@hotmail.com>
2021-09-30 16:58:41 +0100

    added functionality to ORM calculator

--- index.js
+++ index.js
@@ -4,4 +4,39 @@ import {
   calculateLifts,
 } from "./calculator-funcs.js";
 
-console.log(calculateLifts(155));
+const closeIntro = document.querySelector("#intro-close");
+const intro = document.querySelector(".intro");
+
+closeIntro.addEventListener("click", () => {
+  intro.remove();
+});
+
+const weightInput = document.querySelector("#weight");
+const repsInput = document.querySelector("#reps");
+
+let maxWeight;
+let maxReps;
+
+weightInput.addEventListener("input", () => {
+  maxWeight = weightInput.value;
+  const orm = document.querySelector("h4");
+  if (maxWeight > 500) {
+    orm.textContent = `You wish!`;
+  } else {
+    const max = calculateOneRepMax(maxWeight, maxReps);
+    if (max === undefined) return;
+    orm.textContent = `Your estimated 1RM is ${max}KG`;
+  }
+});
+
+repsInput.addEventListener("input", () => {
+  maxReps = repsInput.value;
+  const orm = document.querySelector("h4");
+  if (maxReps > 15) {
+    orm.textContent = `For accuracy, reps should be below 15`;
+  } else {
+    const max = calculateOneRepMax(maxWeight, maxReps);
+    if (max === undefined) return;
+    orm.textContent = `Your estimated 1RM is ${max}KG`;
+  }
+});
